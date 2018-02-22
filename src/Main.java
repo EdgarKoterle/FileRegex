7369ff35145ac910f08a417a445d0887c7d897a9
EdgarKoterle <EdgarKoterle@akademiasovy.sk>
2018-02-22 08:33:46 +0100

    Added writing to file

--- src/Main.java
+++ src/Main.java
@@ -6,41 +6,62 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        Pattern pattSPZ=Pattern.compile("(B(A|B|C|J|L|N|R|S|Y)|CA|D(K|S|T)|G(A|L)|H(C|E)|IL|K(A|I|E|K|M|N|S)|L(E|C|M|V)|M(A|I|L|T|Y)|N(I|O|M|R|Z)|P(B|D|E|O|K|N|P|T|U|V)|R(A|K|S|V)|S(A|B|C|E|I|K|L|O|N|P|V)|T(A|C|N|O|R|S|T|V)|V(K|T)|Z(A|C|H|I|M|V))([ ]{0,1})([0-9]{3})([A-Z]{2})");
-        Pattern pattRC=Pattern.compile("([0-9]{2})(01|02|03|04|05|06|07|08|09|10|11|12|51|52|53|54|55|56|57|58|59|60|61|62)(([0]{1}[1-9]{1})|([1-2]{1}[0-9]{1})|([3]{1}[0-1]{1}))/([0-9]{3,4})");
-        Pattern pattMAC=Pattern.compile("([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})");
-        BufferedReader r=new BufferedReader(new FileReader("rawText.txt"));
-        String line;
-
-        while ((line=r.readLine()) != null)
+        try
         {
-            Matcher SPZ=pattSPZ.matcher(line);
-            Matcher RC=pattRC.matcher(line);
-            Matcher MAC=pattMAC.matcher(line);
+            Pattern pattSPZ=Pattern.compile("(B(A|B|C|J|L|N|R|S|Y)|CA|D(K|S|T)|G(A|L)|H(C|E)|IL|K(A|I|E|K|M|N|S)|L(E|C|M|V)|M(A|I|L|T|Y)|N(I|O|M|R|Z)|P(B|D|E|O|K|N|P|T|U|V)|R(A|K|S|V)|S(A|B|C|E|I|K|L|O|N|P|V)|T(A|C|N|O|R|S|T|V)|V(K|T)|Z(A|C|H|I|M|V))([ ]{0,1})([0-9]{3})([A-Z]{2})");
+            Pattern pattRC=Pattern.compile("([0-9]{2})(01|02|03|04|05|06|07|08|09|10|11|12|51|52|53|54|55|56|57|58|59|60|61|62)(([0]{1}[1-9]{1})|([1-2]{1}[0-9]{1})|([3]{1}[0-1]{1}))/([0-9]{3,4})");
+            Pattern pattMAC=Pattern.compile("([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})");
+            BufferedReader r=new BufferedReader(new FileReader("rawText.txt"));
+            String line;
+            FileWriter writeSPZ=new FileWriter("SPZ.txt");
+            FileWriter writeRC=new FileWriter("RC.txt");
+            FileWriter writeMAC=new FileWriter("MAC.txt");
+            System.getProperty("line.separator");
 
-            while (SPZ.find())
+            while ((line=r.readLine()) != null)
             {
-                int start=SPZ.start(0);
-                int end=SPZ.end(0);
+                Matcher matchSPZ=pattSPZ.matcher(line);
+                Matcher matchRC=pattRC.matcher(line);
+                Matcher matchMAC=pattMAC.matcher(line);
 
-                System.out.println(line.substring(start, end));
+                while (matchSPZ.find())
+                {
+                    int start=matchSPZ.start(0);
+                    int end=matchSPZ.end(0);
+
+                    writeSPZ.write(line.substring(start, end));
+                    writeSPZ.write(System.getProperty("line.separator"));
+                }
+
+                while (matchRC.find())
+                {
+                    int start=matchRC.start(0);
+                    int end = matchRC.end(0);
+
+                    writeRC.write(line.substring(start, end));
+                    writeRC.write(System.getProperty("line.separator"));
+                }
+
+                while (matchMAC.find())
+                {
+                    int start=matchMAC.start(0);
+                    int end=matchMAC.end(0);
+
+                    writeMAC.write(line.substring(start, end));
+                    writeMAC.write(System.getProperty("line.separator"));
+                }
             }
 
-            while (RC.find())
-            {
-                int start=RC.start(0);
-                int end=RC.end(0);
+            writeSPZ.close();
+            writeRC.close();
+            writeMAC.close();
 
-                System.out.println(line.substring(start, end));
-            }
+            System.out.println("Success");
+        }
 
-            while (MAC.find())
-            {
-                int start=MAC.start(0);
-                int end=MAC.end(0);
-
-                System.out.println(line.substring(start, end));
-            }
+        catch (IOException e)
+        {
+            e.getMessage();
         }
     }
 }
